0ad2181e02844d86c892b545b1c862549309c2da
abhimanyu <abhimanyusethia12@gmail.com>
2023-03-27 08:36:09 +0530

    added Richardson Romberg

--- logistic_reg.r
+++ logistic_reg.r
@@ -1,6 +1,8 @@
 library(ggplot2)
+library(pracma)
+
 ##################
-# GENERATING DATA 
+# GENERATING DATA
 ##################
 N <- 10 #sample size
 d <- 1 # number of features
@@ -9,11 +11,9 @@ X <- rbind(matrix(rnorm(N*d), nrow=d, ncol=N), c(rep(1,N))) #x = (d+1)*N matrix 
 theta.star = matrix(sample(1:10,d+1,replace = TRUE))
 prob <- sigmoid(t(theta.star) %*% X)
 y <- rbinom(N,1, prob)
-#windows()
-#plot(X[1,],y, main='Plotting generated data')
 
 ##################
-# COST FUNCTION
+# HELPER FUNCTIONS
 ##################
 sigmoid <- function(z) {
   1/(1+exp(-z))
@@ -29,168 +29,138 @@ loss_grad <- function(X,y,theta){
     delta <- X %*% t(error) / length(y)
     delta
 }
-convert_to_df <- function(theta_history, theta0_lower, theta0_upper, theta1_lower, theta1_upper) {
-    theta_history_df <- data.frame(theta0 = sapply(theta_history, `[`, 1),
-                                theta1 = sapply(theta_history, `[`, 2))
-    theta_history_df$loss <- apply(theta_history_df, 1, function(b) {
-        cost(X, y, b)
-    })
-
-    return (theta_history_df)
-}
-####################
-# PLOTTING FUNCTIONS
-####################
-xy_plot <- function(theta_history, theta_final) {
-    windows()
-    plot(X[1,],y, col=rgb(0.2,0.4,0.6,0.4),xlab="x[1]", ylab="y", main='Linear regression by gradient descent')
-    abline(a=theta_final[2],b=theta_final[1], col='blue')
-    abline(a=theta.star[2],b=theta.star[1], col='green')
-
-    for (i in c(1,3,6,10,14,seq(20,length(theta_history),by=10))) {
-        abline(a=theta_history[[i]][2],b=theta_history[[i]][1], col=rgb(0.8,0,0,0.3))
-        Sys.sleep(0.1)
-    }
-}
 
 ##################
 # HYPERPARAMETERS 
 ##################
-num_iters <- 300 #number of iterations
-alpha <- 0.01 #learning rate
-#gamma <- 0.02 #momentum coefficient
 theta_initial <- matrix(rep(1,d+1), nrow=d+1)
 batch_size <- length(y)
-#theta0_lower <- -1
-#theta1_lower <- -1
-#theta0_upper <- 3.5
-#theta1_upper <- 2.5
+iters_sample <- 50
+iters<- as.integer(logspace(1, 6, n=iters_sample))
 
-##########################
-# VANILLA GRADIENT DESCENT
-##########################
 
-vgd <- function(X,y,theta,batch_size, num_iters,alpha){
-    theta_history <- list(num_iters)
-    theta_history[[1]] <- theta
-    start <- Sys.time()
-    for(i in 2:num_iters){
+vgd <- function(X,y,theta,batch_size,alpha,iters){
+    theta_history <- list()
+    theta_averaged_history <- list()
+    theta <- theta_initial
+    theta_averaged <- theta_initial
+    index <- 1
+    #start <- Sys.time()
+    for(i in 1:iters[length(iters)]){
         sample_indices <- sample(1:length(y), size = batch_size, replace = TRUE)
         X_batch <- X[,sample_indices]
         y_batch <- y[sample_indices]
         gradient <- loss_grad(X_batch,y_batch,theta)
         theta <- theta - alpha *gradient
-        theta_history[[i]] <- theta
+        theta_averaged <- (theta_averaged*(i-1)+theta)/i
+        if(i %in% iters){
+            theta_history[[index]] <- theta
+            theta_averaged_history[[index]] <- theta_averaged
+            index <- index +1
+        }
     }
-    end <- Sys.time()
-    return(list(theta = theta,theta_history = theta_history, time = end-start))
+    return(list(nonaveraged = theta_history, averaged = theta_averaged_history))
 }
-#batch_size <- 1
-#num_iters <- 100
-vgd_output <- vgd(X,y,theta_initial,batch_size,num_iters,alpha)
-vgd_output <- vgd(X,y,theta_initial,batch_size,100,0.01)
-foolog <- 10^(seq(from = 0, to = 8, length.out = 50))
-foolog
-cost(X,y,theta.star)
-cost(X,y,vgd_output$theta)
 
-for(k in )
+vgd_output <- vgd(X,y,theta_initial,batch_size,0.01,iters)
+theta_0.01_nonavg <- vgd_output$nonaveraged
+theta_0.01_avg <- vgd_output$averaged
+vgd_output <- vgd(X,y,theta_initial,batch_size,0.1,iters)
+theta_0.1_nonavg <- vgd_output$nonaveraged
+theta_0.1_avg <- vgd_output$averaged
+
+vgd_variablelr <- function(X,y,theta,batch_size,alpha,iters){
+    theta_history <- list()
+    theta <- theta_initial
+    theta_averaged_history <- list()
+    theta_averaged <- theta_initial
+
+    index <- 1
+    #start <- Sys.time()
+    for(i in 1:iters[length(iters)]){
+        sample_indices <- sample(1:length(y), size = batch_size, replace = TRUE)
+        X_batch <- X[,sample_indices]
+        y_batch <- y[sample_indices]
+        gradient <- loss_grad(X_batch,y_batch,theta)
+        theta <- theta - alpha *gradient/sqrt(i)
+        theta_averaged <- (theta_averaged*(i-1)+theta)/i
+        if(i %in% iters){
+            theta_history[[index]] <- theta
+            theta_averaged_history[[index]] <- theta_averaged
+            index <- index +1
+        }
+        else{}
+    }
+    return(list(nonaveraged = theta_history, averaged = theta_averaged_history))
+}
+theta_0.1varlr_output <- vgd(X,y,theta_initial,batch_size,0.1,iters)
+theta_0.1varlr_nonavg <- theta_0.1varlr_output$nonaveraged
+theta_0.1varlr_avg <- theta_0.1varlr_output$averaged
+
+vgd_richardsonromberg <- function(X,y,theta_initial,batch_size,alpha,iters){
+    theta_history <- list()
+    theta_averaged_history <- list()
+    theta_gamma <- theta_initial
+    theta_2gamma <- theta_initial
+    theta_gamma_avg <- theta_initial
+    theta_2gamma_avg <- theta_initial
+    index <- 1
+    #start <- Sys.time()
+    for(i in 1:iters[length(iters)]){
+        sample_indices <- sample(1:length(y), size = batch_size, replace = TRUE)
+        X_batch <- X[,sample_indices]
+        y_batch <- y[sample_indices]
+        gradient <- loss_grad(X_batch,y_batch,theta_gamma)
+        theta_gamma <- theta_gamma - alpha *gradient
+        theta_gamma_avg <- (theta_gamma_avg*i + theta_gamma)/i
+        gradient <- loss_grad(X_batch,y_batch,theta_2gamma)
+        theta_2gamma <- theta_2gamma - 2*alpha *gradient
+        theta_2gamma_avg <- (theta_2gamma_avg*i + theta_2gamma)/i
+        if(i %in% iters){
+            theta_history[[index]] <- theta_2gamma - theta_gamma
+            theta_averaged_history[[index]] <- theta_2gamma_avg - theta_gamma_avg
+            index <- index +1
+        }
+    }
+    return (list(nonaveraged = theta_history, averaged = theta_averaged_history))
+}
+
+theta_rr_output <- vgd_richardsonromberg(X,y,theta_initial,batch_size,0.1,iters)
+theta_0.1rr_nonavg <- theta_rr_output$nonaveraged
+theta_0.1rr_avg <- theta_rr_output$averaged
 
 
-vgd_output$theta
-theta.star
-vgd_gd <- vgd(X,y,theta_initial,length(y),num_iters,alpha)
-vgd_sgd <- vgd(X,y,theta_initial,1,num_iters,alpha)
+##PLOTTING
+losses_0.01 <- list()
+losses_0.1 <- list()
+losses_0.1varlr <- list()
+losses_0.01_avg <- list()
+losses_0.1_avg <- list()
+losses_0.1varlr_avg <- list()
+losses_0.1rr <- list()
+losses_0.1rr_avg <- list()
+for(i in 1:iters_sample){
+    losses_0.01[i] <- cost(X,y,theta_0.01_nonavg[[i]])
+    losses_0.1[i] <- cost(X,y,theta_0.1_nonavg[[i]]) 
+    losses_0.1varlr[i] <- cost(X,y,theta_0.1varlr_nonavg[[i]])
+    losses_0.01_avg[i] <- cost(X,y,theta_0.01_avg[[i]])
+    losses_0.1_avg[i] <- cost(X,y,theta_0.1_avg[[i]]) 
+    losses_0.1varlr_avg[i] <- cost(X,y,theta_0.1varlr_avg[[i]])
+    losses_0.1rr[i] <- cost(X,y,theta_0.1rr_nonavg[[i]])
+    losses_0.1rr_avg[i] <- cost(X,y,theta_0.1rr_avg[[i]])
+}
 
-
-#PLOTs
-xy_plot(vgd_output$theta_history,vgd_output$theta)
-#contour_plotting(vgd_output$theta_history,theta0_lower, theta0_upper,theta1_lower,theta1_upper)
-#sgd1 <- vgd(X,y,theta_initial,1,num_iters,alpha)
-#gd1 <- vgd(X,y,theta_initial,length(y),num_iters,alpha)
-#gdvssgd_contour_plotting(gd_output = gd1,sgd_output = sgd1,theta0_lower,theta0_upper,theta1_lower,theta1_upper,"Vanilla")
-
-##################
-# COMPILED RESULTS
-##################
-compiled_results <- data.frame(
-    methods = c('Vanilla','Momentum','NAG','Adagrad','RMSprop','Adam','Corrected Adam'),
-    time = c(vgd_output$time,momentum_output$time,nag_output$time,adagrad_output$time,rmsprop_output$time,adam_output$time,corrected_adam_output$time),
-    loss = c(cost(X,y,vgd_output$theta),cost(X,y,momentum_output$theta),cost(X,y,nag_output$theta),cost(X,y,adagrad_output$theta),cost(X,y,rmsprop_output$theta),cost(X,y,adam_output$theta),cost(X,y,corrected_adam_output$theta))
-)
-print(compiled_results)
-print(paste("Actual Cost: ",cost(X,y, theta.star)))
-
-##################
-# COMMON PLOT
-##################
-theta_grid <- expand.grid(theta0 = seq(theta0_lower, theta0_upper, length.out = 500),
-                            theta1 = seq(theta1_lower, theta1_upper, length.out = 500))
-theta_grid$loss <- apply(theta_grid, 1, function(b) {
-    cost(X, y, b)
-})
-theta.star_df <- data.frame(theta0 = theta.star[1], theta1 = theta.star[2])
-theta.star_df$loss <- apply(theta.star_df, 1, function(b) {
-    cost(X, y, b)
-})
-
-
-vgd_df <- convert_to_df(vgd_output$theta_history, theta0_lower, theta0_upper, theta1_lower, theta1_upper)
-momentum_df <- convert_to_df(momentum_output$theta_history, theta0_lower, theta0_upper, theta1_lower, theta1_upper)
-nag_df <- convert_to_df(nag_output$theta_history, theta0_lower, theta0_upper, theta1_lower, theta1_upper)
-adagrad_df <- convert_to_df(adagrad_output$theta_history, theta0_lower, theta0_upper, theta1_lower, theta1_upper)
-rmsprop_df <- convert_to_df(rmsprop_output$theta_history, theta0_lower, theta0_upper, theta1_lower, theta1_upper)
-adam_df <- convert_to_df(adam_output$theta_history, theta0_lower, theta0_upper, theta1_lower, theta1_upper)
-corrected_adam_df <- convert_to_df(corrected_adam_output$theta_history, theta0_lower, theta0_upper, theta1_lower, theta1_upper)
-
-color_maps = c(
-    "VGD" = "blue", 
-    "Momentum" = "yellow",
-    "NAG" = "orange",
-    "Adagrad" = "purple",
-    "RMSProp" = "pink",
-    "Adam" = "brown",
-    "Corrected Adam" = "maroon",
-    "Actual Theta" = "green"
-)
 windows()
-ggplot(theta_grid, aes(x = theta0, y = theta1, z = loss)) +
-  geom_contour(color = "red")+
-  geom_point(data = vgd_df, aes(x = theta0, y = theta1, color = "VGD"), show.legend = TRUE) +
-  geom_point(data = momentum_df, aes(x = theta0, y = theta1, color = "Momentum"), show.legend = TRUE) +
-  geom_point(data = nag_df,aes(x = theta0, y = theta1, color = "NAG"), show.legend = TRUE) +
-  geom_point(data = adagrad_df,aes(x = theta0, y = theta1, color = "Adagrad"), show.legend = TRUE) +
-  geom_point(data = rmsprop_df,aes(x = theta0, y = theta1, color = "RMSProp"), show.legend = TRUE) +
-  geom_point(data = adam_df,aes(x = theta0, y = theta1, color = "Adam"), show.legend = TRUE) +
-  geom_point(data = corrected_adam_df,aes(x = theta0, y = theta1, color = "Corrected Adam"), show.legend = TRUE) +
-  geom_point(data = theta.star_df,aes(x = theta0, y = theta1, color = "Actual Theta"), show.legend = TRUE)+
-  labs(x = expression(theta[0]),
-       y = expression(theta[1]),
-       title = "Contour Plot of Loss Function",
-       color = "Optimization Algorithms") +
-  scale_x_continuous(limits = c(theta0_lower, theta0_upper)) +
-  scale_y_continuous(limits = c(theta1_lower, theta1_upper)) +
-  scale_color_manual(values = color_maps)
+#plot(log10(iters),log10(as.numeric(losses_0.01)),pch = 16, col = "blue",xlab = "log_{10}k", ylab = "log_{10}loss", main="Scatter Plots for Various LR")
+points(log10(iters),log10(as.numeric(losses_0.01_avg)), pch = 16, col = "violet")
+plot(log10(iters),log10(as.numeric(losses_0.1)),pch = 16, col = "red",xlab = "log_{10}k", ylab = "log_{10}loss", main="Scatter Plots for Various LR")
+#points(log10(iters),log10(as.numeric(losses_0.1)), pch = 16, col = "red")
+points(log10(iters),log10(as.numeric(losses_0.1_avg)), pch = 16, col = "maroon")
+#points(log10(iters),log10(as.numeric(losses_0.1varlr)), pch = 16, col = "yellow")
+#points(log10(iters),log10(as.numeric(losses_0.1varlr_avg)), pch = 16, col = "green")
+points(log10(iters),log10(as.numeric(losses_0.1rr)), pch = 16, col = "yellow")
+points(log10(iters),log10(as.numeric(losses_0.1rr_avg)), pch = 16, col = "green")
 
-
-# PLOT: Cost vs Theta[1]
-plot(x=NA, y=NA, xlim=c(0,4), ylim=c(0,7), xlab="Theta [1]", ylab="Cost", main="Cost vs Theta[1] plot")
-for (i in c(seq(1,num_iters,by=10))) {
-    points(x=theta_history[[i]][1], y=cost_history[i], pch=20)
-    Sys.sleep(0.1)
-}
-
-# PLOT: Cost vs Theta[2]
-plot(x=NA, y=NA, xlim=c(0,2.2), ylim=c(0,7), xlab="Theta [2]", ylab="Cost", main="Cost vs Theta[2] plot")
-for (i in c(seq(1,num_iters,by=10))) {
-    points(x=theta_history[[i]][2], y=cost_history[i], pch=20)
-    Sys.sleep(0.1)
-}
-
-
-#################
-# EXPORT DATASET TO CSV
-##################
-dataset <- as.data.frame(X)
-dataset[nrow(dataset) + 1,] <- y
-write.csv(dataset, "100samples_lr_3_2.csv", row.names=FALSE)
+#legend("topright", legend = c("Alpha = 0.01", "Alpha = 0.1", "Alpha = 0.1/k","Alpha = 0.01 Avg", "Alpha = 0.1 Avg", "Alpha = 0.1/k Avg"), col = c("blue", "red","yellow","violet","maroon","green"), pch = 16)
+#legend("topright", legend = c("Alpha = 0.01", "Alpha = 0.1", "Alpha = 0.01 Avg", "Alpha = 0.1 Avg", "Richardson with Alpha = 0.1","Richardson Averaged with Alpha = 0.1"), col = c("blue", "red","violet","maroon", "yellow", "green"), pch = 16)
+legend("bottomleft", legend = c("Alpha = 0.1", "Alpha = 0.1 Avg", "Richardson with Alpha = 0.1","Richardson Averaged with Alpha = 0.1"), col = c( "red","maroon", "yellow", "green"), pch = 16)
